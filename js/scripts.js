5e32d6fce13c740286059b1662952568a8e03db5
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-28 20:11:02 -0800

    Display currentPlayer Icon when square is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function Game(Board) {
-  this.currentPlayer = 1;
+  this.currentPlayer = 2;
   this.players = [];
   this.board = Board;
   this.turn = 0;
@@ -15,8 +15,33 @@ function Player(name, position) {
   this.score = 0;
 }
 
-function checkForWinner(board) {
+function addPlayerIcon(currentPlayer, index, board) {
+  if(currentPlayer === 1) {
+    board[index] = "X";
+    console.log(board)
+  } else if(currentPlayer === 2) {
+    board[index] = "O";
+    console.log(board);
+  } else {
+    console.log("Error: Something went wrong in addPlayerIcon()");
+  }
+}
 
+function changeGameState(Game) {
+  if(Game.currentPlayer === 1) {
+    Game.currentPlayer = 2;
+  } else if(Game.currentPlayer === 2) {
+    Game.currentPlayer = 1;
+  } else {
+    console.log("Error: Something went wrong in changeGameState()");
+  }
+}
+
+//Front-End Logic
+function displayBoard(board) {
+  for(let i = 0; i < 9; i++) {
+    $("div#" + i).html(board[i]);
+  }
 }
 
 $(document).ready(function() {
@@ -26,7 +51,8 @@ $(document).ready(function() {
     $("#nameScreen").show();
   })
   $("form#form").submit(function(event) {
-    var board = [[1,1,1], [1,1,1], [1,1,1]];
+    event.preventDefault();
+    var board = ["","","","","","","","",""];
     var newGame = new Game(board);
     var p1Name = $("#name1").val();
     var p2Name = $("#name2").val();
@@ -34,5 +60,14 @@ $(document).ready(function() {
     var p2 = new Player(p2Name, 2);
     newGame.addPlayer(p1);
     newGame.addPlayer(p2);
+    for(let i = 0; i < 9; i++) {
+      $("div#" + i).click(function(event) {
+        event.preventDefault();
+        addPlayerIcon(newGame.currentPlayer, i, board);
+        changeGameState(newGame);
+        displayBoard(board);
+      })
+    }
+    console.log(board);
   })
 })
